812c82c95ee5f54373f6c51a8c9fe21daab59546
reatter <michael.lannert@gmail.com>
2018-02-26 19:12:56 +0100

    uid StoneWall

--- src/entities/tiles/StoneWall.java
+++ src/entities/tiles/StoneWall.java
@@ -4,6 +4,11 @@ import entities.abstracts.GameTile;
 
 public class StoneWall extends GameTile implements Passable {
 
+    /**
+     * long serialVersionUID
+     */
+    private static final long serialVersionUID = -2130438095293803021L;
+
     public StoneWall(long id, int x, int y, int depth) {
 	super(id, x, y, depth);
 	// TODO Auto-generated constructor stub
